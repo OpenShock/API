d74156859ca8f71a2cef2e4468fe02a6fcb0f803
Luca <lucper@outlook.de>
2023-03-01 02:42:27 +0100

    Efcore scaffold update

---
+++ Common/ShockLinkDb/DeviceShare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShockLink.Common.ShockLinkDb;
+
+public partial class DeviceShare
+{
+    public Guid DeviceId { get; set; }
+
+    public Guid SharedWith { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public virtual Device Device { get; set; } = null!;
+
+    public virtual User SharedWithNavigation { get; set; } = null!;
+}
