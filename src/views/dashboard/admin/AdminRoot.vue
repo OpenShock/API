0faa012b8ae7c37efc0f42080d70dd06e88b993b
Luca <lucper@outlook.de>
2023-03-03 04:57:34 +0100

    All of my intial conent chages, features login, dashboard, api integration and co

---
+++ src/views/dashboard/admin/AdminRoot.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="base-wrap">
+  <router-view v-slot="{ Component }">
+    <transition name="component-fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
+</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$store.dispatch('setNewNav', [
+      {
+        routerLink: '/admin/users',
+        html: '<i class="fas fa-search"></i>Users'
+      }
+    ]);
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
