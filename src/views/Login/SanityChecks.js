34346e087c8321efc28d68279bb773e7a6450cca
Luca <lucper@outlook.de>
2023-03-25 07:42:07 +0100

    min 12 pw lenght

--- src/views/Login/SanityChecks.js
+++ src/views/Login/SanityChecks.js
@@ -25,7 +25,7 @@ class SanityChecks {
 		let arr = [];
 
 		if (password.length <= 8) arr.push("Minimum length of 8 characters");
-		if (password.length >= 256) arr.push("Maximum length of 128 characters");
+		if (password.length >= 256) arr.push("Maximum length of 256 characters");
 		if(!this.hasLowerCase(password)) arr.push("One lowercase character");
 		if(!this.hasUpperCase(password)) arr.push("One uppercase character");
 		if(!this.hasNumber(password)) arr.push("One number");
