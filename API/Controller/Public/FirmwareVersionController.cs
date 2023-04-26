c6c43f9fa8d4dfde17e6189462ab75f5bb7b9b6d
LucHeart <luca@perugia.ovh>
2023-04-27 00:13:15 +0200

    Firmware endpoint

---
+++ API/Controller/Public/FirmwareVersionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShockLink.API.Models;
+using ShockLink.Common.Models;
+
+namespace ShockLink.API.Controller.Public;
+
+[ApiController]
+[Route("/{version:apiVersion}/public/firmware/version")]
+public class FirmwareVersionController : ShockLinkControllerBase
+{
+    [HttpGet]
+    public async Task<BaseResponse<FirmwareVersion>> Get()
+    {
+        return new BaseResponse<FirmwareVersion>
+        {
+            Data = new FirmwareVersion
+            {
+                Version = new Version(0, 5, 0, 0),
+                DownloadUri = new Uri("https://cdn.shocklink.net/firmware/shocklink_firmware_0.5.0.0.bin")
+            }
+        };
+    }
+}
---
+++ Common/Models/FirmwareVersion.cs
@@ -0,0 +1,7 @@
+﻿namespace ShockLink.Common.Models;
+
+public class FirmwareVersion
+{
+    public required Version Version { get; set; }
+    public required Uri DownloadUri { get; set; }
+}
