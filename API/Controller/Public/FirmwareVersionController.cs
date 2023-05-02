998286dfbad2d4644917d1ac10d9de55e3969945
LucHeart <luca@perugia.ovh>
2023-05-02 20:05:31 +0200

    Bump firmware version 0.5.2.0

--- API/Controller/Public/FirmwareVersionController.cs
+++ API/Controller/Public/FirmwareVersionController.cs
@@ -16,7 +16,7 @@ public class FirmwareVersionController : ShockLinkControllerBase
             Data = new FirmwareVersion
             {
                 Version = new Version(0, 5, 0, 1),
-                DownloadUri = new Uri("https://cdn.shocklink.net/firmware/shocklink_firmware_0.5.0.1.bin")
+                DownloadUri = new Uri("https://cdn.shocklink.net/firmware/shocklink_firmware_0.5.2.0.bin")
             }
         };
     }
