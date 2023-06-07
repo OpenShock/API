0bb0d85c6975069232392b5160179fa7404309ac
LucHeart <luca@perugia.ovh>
2023-06-07 23:18:43 +0200

    use obj for request pause

--- API/Controller/Shockers/PauseShockerController.cs
+++ API/Controller/Shockers/PauseShockerController.cs
@@ -19,14 +19,19 @@ public class PauseShockersController : AuthenticatedSessionControllerBase
     }
 
     [HttpPost("{id:guid}/pause")]
-    public async Task<BaseResponse<object>> Pause(Guid id, bool pause)
+    public async Task<BaseResponse<bool?>> Pause(Guid id, PauseRequest data)
     {
         var shocker = await _db.Shockers.Where(x => x.Id == id && x.DeviceNavigation.Owner == CurrentUser.DbUser.Id)
             .SingleOrDefaultAsync();
         if (shocker == null)
-            return EBaseResponse<object>("Shocker not found or does not belong to you", HttpStatusCode.NotFound);
-        shocker.Paused = pause;
+            return EBaseResponse<bool?>("Shocker not found or does not belong to you", HttpStatusCode.NotFound);
+        shocker.Paused = data.Pause;
         await _db.SaveChangesAsync();
-        return new BaseResponse<object>("Successfully set pause state");
+        return new BaseResponse<bool?>("Successfully set pause state", data.Pause);
+    }
+    
+    public class PauseRequest
+    {
+        public required bool Pause { get; set; }
     }
 }
