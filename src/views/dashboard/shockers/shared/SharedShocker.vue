0c9423c0c82772d73bdfe16ded7ed923a4240176
LucHeart <luca@perugia.ovh>
2023-06-08 02:56:58 +0200

    Paused for shared

--- src/views/dashboard/shockers/shared/SharedShocker.vue
+++ src/views/dashboard/shockers/shared/SharedShocker.vue
@@ -8,54 +8,56 @@
                 <i class="fa-solid fa-ellipsis-vertical"></i>
             </b-col>
         </b-row>
-        <b-row>
-            <b-container align-items="center" style="margin-top: 15px">
+
+        <div class="content-container">
+            <div v-if="shocker.isPaused" class="paused-text width100">
+                <b-container class="width100">
+                    <b-row align-h="center">
+                        <b-col cols="auto">
+                            <h2>Paused by owner</h2>
+                        </b-col>
+                        <b-col cols="auto">
+                            <i style="font-size: 38px;" class="fa-solid fa-play"></i>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </div>
+            <div class="content-child" :class="shocker.isPaused ? 'paused' : ''">
+                <b-row>
+                    <b-container align-items="center" style="margin-top: 15px">
+                        <b-row align-h="center">
+                            <b-col md="auto" style="width: unset">
+                                <round-slider v-model="shocker.state.intensity" pathColor="#1b1d1e" rangeColor="#8577ef"
+                                    start-angle="315" end-angle="+270" width="30" line-cap="round" radius="75" />
+
+                                <p style="text-align: center;">Intensity</p>
+                            </b-col>
+                            <b-col md="auto" style="width: unset">
+                                <round-slider v-model="shocker.state.duration" pathColor="#1b1d1e" rangeColor="#8577ef"
+                                    start-angle="315" end-angle="+270" line-cap="round" radius="75" width="30" min="0.3"
+                                    max="30" step="0.1" />
+
+                                <p style="text-align: center;">Duration</p>
+                            </b-col>
+                        </b-row>
+                    </b-container>
+                </b-row>
                 <b-row align-h="center">
-                    <b-col md="auto" style="width: unset">
-                        <round-slider
-                            v-model="shocker.state.intensity"
-                            pathColor="#1b1d1e"
-                            rangeColor="#8577ef"
-                            start-angle="315"
-                            end-angle="+270"
-                            width="30"
-                            line-cap="round"
-                            radius="75"
-                            />
-
-                        <p style="text-align: center;">Intensity</p>
+                    <b-col cols="auto" md="auto">
+                        <loading-button style="width: 46px" text="" icon="fa-solid fa-volume-high" :disabled="inProgress"
+                            loadingIcon="fa-solid fa-spinner fa-spin" :loading="inProgress" @click="control(3)" />
                     </b-col>
-                    <b-col md="auto"  style="width: unset">
-                        <round-slider
-                            v-model="shocker.state.duration"
-                            pathColor="#1b1d1e"
-                            rangeColor="#8577ef"
-                            start-angle="315"
-                            end-angle="+270"
-                            line-cap="round"
-                            radius="75"
-                            width="30"
-                            min="0.3"
-                            max="30"
-                            step="0.1"
-                            />
-
-                        <p style="text-align: center;">Duration</p>
+                    <b-col cols="auto" md="auto">
+                        <loading-button style="width: 46px" text="" icon="fa-solid fa-water" :disabled="inProgress"
+                            loadingIcon="fa-solid fa-spinner fa-spin" :loading="inProgress" @click="control(2)" />
+                    </b-col>
+                    <b-col cols="auto" md="auto">
+                        <loading-button style="left: 0; width: 46px" text="" icon="fa-solid fa-bolt" :disabled="inProgress"
+                            loadingIcon="fa-solid fa-spinner fa-spin" :loading="inProgress" @click="control(1)" />
                     </b-col>
                 </b-row>
-            </b-container>
-        </b-row>
-        <b-row align-h="center">
-            <b-col cols="auto" md="auto">
-                <loading-button style="width: 46px" text="" icon="fa-solid fa-volume-high" :disabled="inProgress" loadingIcon="fa-solid fa-spinner fa-spin" :loading="inProgress" @click="control(3)"/>
-            </b-col>
-            <b-col cols="auto" md="auto">
-                <loading-button style="width: 46px" text="" icon="fa-solid fa-water" :disabled="inProgress" loadingIcon="fa-solid fa-spinner fa-spin" :loading="inProgress" @click="control(2)"/>
-            </b-col>
-            <b-col cols="auto" md="auto">
-                <loading-button style="left: 0; width: 46px" text="" icon="fa-solid fa-bolt" :disabled="inProgress" loadingIcon="fa-solid fa-spinner fa-spin" :loading="inProgress" @click="control(1)"/>
-            </b-col>
-        </b-row>
+            </div>
+        </div>
     </b-container>
 </template>
 
@@ -65,80 +67,80 @@ import LoadingButton from '../../../utils/LoadingButton.vue';
 import RoundSlider from 'vue-three-round-slider';
 
 export default {
-  components: { LoadingButton, Loading, RoundSlider },
+    components: { LoadingButton, Loading, RoundSlider },
 
-        props: ["shocker"],
-        data() {
-            return {
-                inProgress: false
-            }
-        },
-        beforeMount() {
+    props: ["shocker"],
+    data() {
+        return {
+            inProgress: false
+        }
+    },
+    beforeMount() {
 
-        },
-        methods: {
-            ellipsis(e) {
-                this.$contextmenu({
-                    theme: utils.isDarkMode() ? 'default dark' : 'default',
-                    x: e.x,
-                    y: e.y,
-                    items: [
-                        { 
-                            label: "Remove share",
-                            icon: 'fa-solid fa-trash',
-                            onClick: () => {
-                                this.delete();
-                            }
+    },
+    methods: {
+        ellipsis(e) {
+            this.$contextmenu({
+                theme: utils.isDarkMode() ? 'default dark' : 'default',
+                x: e.x,
+                y: e.y,
+                items: [
+                    {
+                        label: "Remove share",
+                        icon: 'fa-solid fa-trash',
+                        onClick: () => {
+                            this.delete();
                         }
-                    ]
-                }); 
-            },
-            delete() {
-                this.$swal({
-                    title: 'Unlink share?',
-                    html: `You are remove the share link for shocker <b>${this.shocker.name}</b> with id (${this.shocker.id}).<br>You wont be able to control the shocker after unlinking it.</b>
+                    }
+                ]
+            });
+        },
+        delete() {
+            this.$swal({
+                title: 'Unlink share?',
+                html: `You are remove the share link for shocker <b>${this.shocker.name}</b> with id (${this.shocker.id}).<br>You wont be able to control the shocker after unlinking it.</b>
                     <br><br>Are you sure?`,
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#d33',
-                    cancelButtonColor: 'var(--secondary-seperator-color)',
-                    showLoaderOnConfirm: true,
-                    confirmButtonText: 'Unlink shared shocker',
-                    allowOutsideClick: () => !this.$swal.isLoading(),
-                    preConfirm: async () => {
-                        try {
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: 'var(--secondary-seperator-color)',
+                showLoaderOnConfirm: true,
+                confirmButtonText: 'Unlink shared shocker',
+                allowOutsideClick: () => !this.$swal.isLoading(),
+                preConfirm: async () => {
+                    try {
                         const res = await apiCall.makeCall('DELETE', `1/shares/${this.shocker.id}`);
                         if (res.status !== 200) {
                             throw new Error(res.statusText);
                         }
 
-                        } catch (err) {
+                    } catch (err) {
                         this.$swal.showValidationMessage(`Request failed: ${err}`)
-                        }
-                    },
-                    }).then(async (result) => {
-                        if (result.isConfirmed) {
-                            this.$swal('Success!', 'Successfully unlinked shared shocker', 'success');
-                            this.emitter.emit('refreshShockers');
-                        }
-                });
-            },
-            control(type) {
-                ws.control([
-                    {
-                        "Id": this.shocker.id,
-                        "Type": type,
-                        "Duration": parseFloat(this.shocker.state.duration) * 1000,
-                        "Intensity": parseInt(this.shocker.state.intensity)
                     }
-                ]);
+                },
+            }).then(async (result) => {
+                if (result.isConfirmed) {
+                    this.$swal('Success!', 'Successfully unlinked shared shocker', 'success');
+                    this.emitter.emit('refreshShockers');
+                }
+            });
+        },
+        control(type) {
+            ws.control([
+                {
+                    "Id": this.shocker.id,
+                    "Type": type,
+                    "Duration": parseFloat(this.shocker.state.duration) * 1000,
+                    "Intensity": parseInt(this.shocker.state.intensity)
+                }
+            ]);
 
-                this.inProgress = true;
+            this.inProgress = true;
 
-                setTimeout(() => this.inProgress = false, this.shocker.state.duration * 1000);
-            }
+            setTimeout(() => this.inProgress = false, this.shocker.state.duration * 1000);
         }
     }
+}
 </script>
 
 <style scoped lang="scss">
@@ -148,6 +150,30 @@ export default {
     border-radius: 10px;
     margin: 10px 0;
     padding: 10px;
+
+    .content-container {
+        position: relative;
+
+        transition: 1s filter linear, 1s -webkit-filter linear;
+
+        .paused-text {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            z-index: 100;
+        }
+
+        .content-child {
+            transition: filter 0.3s ease-in-out;
+            filter: blur(0px);
+        }
+
+        .paused {
+            pointer-events: none;
+            filter: blur(5px);
+        }
+    }
 
     .head {
         border-bottom: solid 2px var(--main-background-color);
@@ -163,7 +189,7 @@ export default {
                 overflow: hidden;
                 margin-bottom: 0;
             }
-        }   
+        }
     }
 
 
