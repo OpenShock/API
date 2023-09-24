0b81ea8e0049dfcd0b9f19b70b267592692da96d
LucHeart <luca@perugia.ovh>
2023-09-24 15:08:36 +0200

    default test image

--- API/Utils/GravatarUtils.cs
+++ API/Utils/GravatarUtils.cs
@@ -9,7 +9,7 @@ public static class GravatarUtils
     {
         Span<byte> tempSpan = stackalloc byte[32];
         SHA256.HashData(Encoding.UTF8.GetBytes(email), tempSpan);
-        return new Uri($"https://www.gravatar.com/avatar/{Convert.ToHexString(tempSpan)}/");
+        return new Uri($"https://www.gravatar.com/avatar/{Convert.ToHexString(tempSpan)}?d=https%3A%2F%2Fsea.zlucplayz.com%2Ff%2F897bd3f9b09945cf8c4f%2F%3Fraw%3D1");
     }
     
 }
