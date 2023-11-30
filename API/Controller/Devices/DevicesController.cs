5457dc7702237dd4452e83b6710f1465cee4fd14
LucHeart <luca@perugia.ovh>
2023-11-30 07:29:31 +0100

    Add endpoint to get LCG info

--- API/Controller/Devices/DevicesController.cs
+++ API/Controller/Devices/DevicesController.cs
@@ -18,11 +18,13 @@ public class DeviceController : AuthenticatedSessionControllerBase
 {
     private readonly OpenShockContext _db;
     private readonly IRedisCollection<DevicePair> _devicePairs;
+    private readonly IRedisCollection<DeviceOnline> _devicesOnline;
 
     public DeviceController(OpenShockContext db, IRedisConnectionProvider provider)
     {
         _db = db;
         _devicePairs = provider.RedisCollection<DevicePair>();
+        _devicesOnline = provider.RedisCollection<DeviceOnline>(false);
     }
 
     [HttpGet]
@@ -37,7 +39,7 @@ public class DeviceController : AuthenticatedSessionControllerBase
             }).ToListAsync();
         return new BaseResponse<IEnumerable<Models.Response.ResponseDevice>>
         {
-           Data = devices
+            Data = devices
         };
     }
 
@@ -53,17 +55,19 @@ public class DeviceController : AuthenticatedSessionControllerBase
                 Token = x.Token
             }).SingleOrDefaultAsync();
         if (device == null)
-            return EBaseResponse<Models.Response.ResponseDeviceWithToken>("Device does not exist", HttpStatusCode.NotFound);
+            return EBaseResponse<Models.Response.ResponseDeviceWithToken>("Device does not exist",
+                HttpStatusCode.NotFound);
         return new BaseResponse<Models.Response.ResponseDeviceWithToken>
         {
             Data = device
         };
     }
-    
+
     [HttpPatch("{id:guid}")]
     public async Task<BaseResponse<object>> Edit(Guid id, DeviceEdit data)
     {
-        var device = await _db.Devices.Where(x => x.Owner == CurrentUser.DbUser.Id && x.Id == id).SingleOrDefaultAsync();
+        var device = await _db.Devices.Where(x => x.Owner == CurrentUser.DbUser.Id && x.Id == id)
+            .SingleOrDefaultAsync();
         if (device == null)
             return EBaseResponse<object>("Device does not exist", HttpStatusCode.NotFound);
 
@@ -75,29 +79,32 @@ public class DeviceController : AuthenticatedSessionControllerBase
             Message = "Successfully updated device"
         };
     }
-        
+
     [HttpPut("{id:guid}")]
     public async Task<BaseResponse<object>> RegenToken(Guid id)
     {
-        var device = await _db.Devices.Where(x => x.Owner == CurrentUser.DbUser.Id && x.Id == id).SingleOrDefaultAsync();
+        var device = await _db.Devices.Where(x => x.Owner == CurrentUser.DbUser.Id && x.Id == id)
+            .SingleOrDefaultAsync();
         if (device == null)
             return EBaseResponse<object>("Device does not exist", HttpStatusCode.NotFound);
 
         device.Token = CryptoUtils.RandomString(256);
 
         var affected = await _db.SaveChangesAsync();
-        if (affected <= 0) return EBaseResponse<object>("Failed to save regenerated token", HttpStatusCode.InternalServerError);
-        
+        if (affected <= 0)
+            return EBaseResponse<object>("Failed to save regenerated token", HttpStatusCode.InternalServerError);
+
         return new BaseResponse<object>
         {
             Message = "Successfully regenerated device token"
         };
     }
-    
+
     [HttpDelete("{id:guid}")]
     public async Task<BaseResponse<object>> Delete(Guid id)
     {
-        var affected = await _db.Devices.Where(x => x.Owner == CurrentUser.DbUser.Id && x.Id == id).ExecuteDeleteAsync();
+        var affected = await _db.Devices.Where(x => x.Owner == CurrentUser.DbUser.Id && x.Id == id)
+            .ExecuteDeleteAsync();
         if (affected <= 0)
             return EBaseResponse<object>("Device does not exist", HttpStatusCode.NotFound);
         return new BaseResponse<object>
@@ -118,7 +125,7 @@ public class DeviceController : AuthenticatedSessionControllerBase
         };
         _db.Devices.Add(device);
         await _db.SaveChangesAsync();
-        
+
         Response.StatusCode = (int)HttpStatusCode.Created;
         return new BaseResponse<Guid>
         {
@@ -136,7 +143,7 @@ public class DeviceController : AuthenticatedSessionControllerBase
         // replace with unlink?
         var existing = await _devicePairs.FindByIdAsync(id.ToString());
         if (existing != null) await _devicePairs.DeleteAsync(existing);
-        
+
         var r = new Random();
         var pairCode = new DevicePair
         {
@@ -148,6 +155,37 @@ public class DeviceController : AuthenticatedSessionControllerBase
         return new BaseResponse<string>
         {
             Data = pairCode.PairCode
+        };
+    }
+
+    /// <summary>
+    /// Get LCG info for a device if it is online and connected to a LCG node
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("{id:guid}/lcg")]
+    public async Task<BaseResponse<string>> GetLcgInfo(Guid id)
+    {
+        // Check if user owns device or has a share
+        var deviceExistsAndYouHaveAccess = await _db.Devices.AnyAsync(x =>
+            x.Id == id && (x.Owner == CurrentUser.DbUser.Id || x.Shockers.Any(y => y.ShockerShares.Any(
+                z => z.SharedWith == CurrentUser.DbUser.Id))));
+        if (!deviceExistsAndYouHaveAccess)
+            return EBaseResponse<string>("Device does not exists or does not belong to you", HttpStatusCode.NotFound);
+        
+        // Check if device is online
+        var online = await _devicesOnline.FindByIdAsync(id.ToString());
+        if (online == null) return EBaseResponse<string>("Device is not online", HttpStatusCode.NotFound);
+        
+        // Check if device is connected to a LCG node
+        if (online.Gateway == null)
+            return EBaseResponse<string>(
+                "Device is online but not connected to a LCG node, you might need to upgrade your firmware to use this feature",
+                HttpStatusCode.PreconditionFailed);
+
+        return new BaseResponse<string>
+        {
+            Data = online.Gateway
         };
     }
 }
