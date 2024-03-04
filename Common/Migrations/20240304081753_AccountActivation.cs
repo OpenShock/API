a123b9dd810b6a29087311f3d97d81de3b34c405
LucHeart <luca@perugia.ovh>
2024-03-04 09:20:47 +0100

    Add DB migration

---
+++ Common/Migrations/20240304081753_AccountActivation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenShock.Common.Migrations
+{
+    /// <inheritdoc />
+    public partial class AccountActivation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users_activation",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    used_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    secret = table.Column<string>(type: "character varying", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_activation_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_activation_user_id",
+                table: "users_activation",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "user_id",
+                table: "password_resets");
+
+            migrationBuilder.DropTable(
+                name: "users_activation");
+        }
+    }
+}
