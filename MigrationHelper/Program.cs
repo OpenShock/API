5a79497af3c371077d6dbbd76ffe9c4885dec38f
LucHeart <luca@perugia.ovh>
2024-03-08 07:25:37 +0100

    Upgrade all nuget packages, add migration helper project

---
+++ MigrationHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenShock.Common.OpenShockDb;
+
+HostBuilder builder = new();
+builder.ConfigureServices(collection =>
+{
+    collection.AddDbContext<OpenShockContext>();
+});
+
+var host = builder.Build();
+host.RunAsync();
