85c6d98a04f92a1ca25150c60976fa6a0fad8895
Luca <luca@perugia.ovh>
2024-03-26 01:16:09 +0100

    Remove redundant correlation id

--- ServicesCommon/ExceptionHandle/ExceptionHandler.cs
+++ ServicesCommon/ExceptionHandle/ExceptionHandler.cs
@@ -11,7 +11,6 @@ public static class ExceptionHandler
 {
     private static readonly ILogger Logger = ApplicationLogging.CreateLogger(typeof(ExceptionHandler));
     private static readonly ILogger LoggerRequestInfo = ApplicationLogging.CreateLogger("RequestInfo");
-    private const string CorrelationIdItem = "CorrelationIdEnricher+CorrelationId";
 
     /// <summary>
     /// Configures two middlewares used to handle exceptions globally.
@@ -39,16 +38,13 @@ public static class ExceptionHandler
                 // This should not be null, otherwise no exception was thrown or some other error in ASP.NET occurred.
                 if (contextFeature == null) return;
                 
-                // Get our logging correlationId from out HttpContext's items.
-                var correlationId = Guid.TryParse(context.Items[CorrelationIdItem]?.ToString(), out var correlationIdGuid) 
-                    ? correlationIdGuid : Guid.Empty;
 
                 // Any other exception has been thrown, return a InternalServerError, always print full request infos.
                 // Side note: Exception logging is done by Microsoft Diagnostics middleware already, so no need to do
                 // it again manually.
                 
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                await PrintRequestInfo(context, correlationId);
+                await PrintRequestInfo(context);
                 
                 
                 var jsonOptions = context.RequestServices.GetRequiredService<IOptions<JsonOptions>>();
@@ -66,8 +62,7 @@ public static class ExceptionHandler
     /// Also contains redundant data like method, path and correlation id for readability purposes.
     /// </summary>
     /// <param name="context"></param>
-    /// <param name="correlationId"></param>
-    private static async Task PrintRequestInfo(HttpContext context, Guid correlationId)
+    private static async Task PrintRequestInfo(HttpContext context)
     {
         // Rewind our body reader, so we can read it again.
         context.Request.Body.Seek(0, SeekOrigin.Begin);
@@ -83,7 +78,7 @@ public static class ExceptionHandler
         {
             Body = await stream.ReadToEndAsync(),
             Headers = headers,
-            CorrelationId = correlationId,
+            TraceId = context.TraceIdentifier,
             Method = context.Request.Method,
             Path = context.Request.Path.Value,
             Query = queryParams
