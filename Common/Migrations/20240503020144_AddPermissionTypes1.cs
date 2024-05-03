401e5a57c149b1f8af37b5eca715d18b6f2a88c4
Luca <luca@perugia.ovh>
2024-05-03 04:03:08 +0200

    Add migration

---
+++ Common/Migrations/20240503020144_AddPermissionTypes1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenShock.Common.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPermissionTypes1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:CollationDefinition:public.ndcoll", "und-u-ks-level2,und-u-ks-level2,icu,False")
+                .Annotation("Npgsql:Enum:control_type", "sound,vibrate,shock,stop")
+                .Annotation("Npgsql:Enum:ota_update_status", "started,running,finished,error,timeout")
+                .Annotation("Npgsql:Enum:password_encryption_type", "pbkdf2,bcrypt_enhanced")
+                .Annotation("Npgsql:Enum:permission_type", "shockers.use,shockers.edit,shockers.pause,devices.edit")
+                .Annotation("Npgsql:Enum:rank_type", "user,support,staff,admin,system")
+                .Annotation("Npgsql:Enum:shocker_model_type", "caiXianlin,petTrainer,petrainer998DR")
+                .OldAnnotation("Npgsql:CollationDefinition:public.ndcoll", "und-u-ks-level2,und-u-ks-level2,icu,False")
+                .OldAnnotation("Npgsql:Enum:control_type", "sound,vibrate,shock,stop")
+                .OldAnnotation("Npgsql:Enum:ota_update_status", "started,running,finished,error,timeout")
+                .OldAnnotation("Npgsql:Enum:password_encryption_type", "pbkdf2,bcrypt_enhanced")
+                .OldAnnotation("Npgsql:Enum:permission_type", "shockers.use")
+                .OldAnnotation("Npgsql:Enum:rank_type", "user,support,staff,admin,system")
+                .OldAnnotation("Npgsql:Enum:shocker_model_type", "caiXianlin,petTrainer,petrainer998DR");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "valid_until",
+                table: "api_tokens",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:CollationDefinition:public.ndcoll", "und-u-ks-level2,und-u-ks-level2,icu,False")
+                .Annotation("Npgsql:Enum:control_type", "sound,vibrate,shock,stop")
+                .Annotation("Npgsql:Enum:ota_update_status", "started,running,finished,error,timeout")
+                .Annotation("Npgsql:Enum:password_encryption_type", "pbkdf2,bcrypt_enhanced")
+                .Annotation("Npgsql:Enum:permission_type", "shockers.use")
+                .Annotation("Npgsql:Enum:rank_type", "user,support,staff,admin,system")
+                .Annotation("Npgsql:Enum:shocker_model_type", "caiXianlin,petTrainer,petrainer998DR")
+                .OldAnnotation("Npgsql:CollationDefinition:public.ndcoll", "und-u-ks-level2,und-u-ks-level2,icu,False")
+                .OldAnnotation("Npgsql:Enum:control_type", "sound,vibrate,shock,stop")
+                .OldAnnotation("Npgsql:Enum:ota_update_status", "started,running,finished,error,timeout")
+                .OldAnnotation("Npgsql:Enum:password_encryption_type", "pbkdf2,bcrypt_enhanced")
+                .OldAnnotation("Npgsql:Enum:permission_type", "shockers.use,shockers.edit,shockers.pause,devices.edit")
+                .OldAnnotation("Npgsql:Enum:rank_type", "user,support,staff,admin,system")
+                .OldAnnotation("Npgsql:Enum:shocker_model_type", "caiXianlin,petTrainer,petrainer998DR");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "valid_until",
+                table: "api_tokens",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+    }
+}
