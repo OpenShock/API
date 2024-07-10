d6ff2c8cdbbc488ad803dd94fe1767cb17878a02
Luca <luca@perugia.ovh>
2024-07-10 22:40:43 +0200

    Add migration

---
+++ Common/Migrations/20240710204029_DefaultValueForTokenLastUsed.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenShock.Common.Migrations
+{
+    /// <inheritdoc />
+    public partial class DefaultValueForTokenLastUsed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_used",
+                table: "api_tokens",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "'-infinity'::timestamp without time zone",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_used",
+                table: "api_tokens",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "'-infinity'::timestamp without time zone");
+        }
+    }
+}
