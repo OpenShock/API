c87bd43ded8d841f0239305682e130726fae4488
hhvrc <github@heavenvr.tech>
2024-09-13 15:10:50 +0200

    Add support to register cron jobs via attributes

--- Cron/Jobs/OtaTimeoutJob.cs
+++ Cron/Jobs/OtaTimeoutJob.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OpenShock.Common.Models;
 using OpenShock.Common.OpenShockDb;
+using OpenShock.Cron.Attributes;
 
 namespace OpenShock.Cron.Jobs;
 
+[CronJob("0 */1 * * * ?")]
 public sealed class OtaTimeoutJob
 {
     private readonly OpenShockContext _db;
