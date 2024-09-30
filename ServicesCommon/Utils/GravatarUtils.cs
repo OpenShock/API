77e420ac0b70f26388d1f72702038dbcc2f1f732
hhvrc <github@heavenvr.tech>
2024-09-30 11:38:35 +0200

    Fix gravatar default url

--- ServicesCommon/Utils/GravatarUtils.cs
+++ ServicesCommon/Utils/GravatarUtils.cs
@@ -1,16 +1,19 @@
-﻿using System.Security.Cryptography;
+﻿using OpenShock.Common.OpenShockDb;
+using System.Security.Cryptography;
 using System.Text;
-using OpenShock.Common.OpenShockDb;
+using System.Web;
 
 namespace OpenShock.ServicesCommon.Utils;
 
 public static class GravatarUtils
 {
+    private static readonly string DefaultImageUrl = HttpUtility.UrlEncode("https://openshock.app/static/images/Icon512.png");
+
     public static Uri GetImageUrl(string email)
     {
         Span<byte> tempSpan = stackalloc byte[32];
         SHA256.HashData(Encoding.UTF8.GetBytes(email), tempSpan);
-        return new Uri($"https://www.gravatar.com/avatar/{Convert.ToHexString(tempSpan).ToLowerInvariant()}?d=https%3A%2F%2Fshocklink.net%2Fstatic%2Fimages%2FIcon512.png");
+        return new Uri($"https://www.gravatar.com/avatar/{Convert.ToHexString(tempSpan).ToLowerInvariant()}?d={DefaultImageUrl}");
     }
 
     public static Uri GetImage(this User user) => GetImageUrl(user.Email);
