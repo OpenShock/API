bbb62ab899622dc47e9b649e9c0a3f600c49d28d
hhvrc <github@heavenvr.tech>
2024-09-30 23:27:38 +0200

    Fall back to prefetched IPs

--- Common/Utils/TrustedProxiesFetcher.cs
+++ Common/Utils/TrustedProxiesFetcher.cs
@@ -21,14 +21,48 @@ public static class TrustedProxiesFetcher
 
     private static async Task<string[]> FetchCloudflareIPs()
     {
-        using var client = new HttpClient();
+        try
+        {
+            using var client = new HttpClient();
 
-        var v4Task = FetchCloudflareIPsV4(client);
-        var v6Task = FetchCloudflareIPsV6(client);
+            var v4Task = FetchCloudflareIPsV4(client);
+            var v6Task = FetchCloudflareIPsV6(client);
 
-        await Task.WhenAll(v4Task, v6Task);
+            await Task.WhenAll(v4Task, v6Task);
 
-        return [.. v4Task.Result, .. v6Task.Result];
+            return [.. v4Task.Result, .. v6Task.Result];
+        }
+        catch (Exception)
+        {
+            // Fetched 00:00:00 30/09/2024 UTC
+            return [
+                // IPv4
+                "173.245.48.0/20",
+                "103.21.244.0/22",
+                "103.22.200.0/22",
+                "103.31.4.0/22",
+                "141.101.64.0/18",
+                "108.162.192.0/18",
+                "190.93.240.0/20",
+                "188.114.96.0/20",
+                "197.234.240.0/22",
+                "198.41.128.0/17",
+                "162.158.0.0/15",
+                "104.16.0.0/13",
+                "104.24.0.0/14",
+                "172.64.0.0/13",
+                "131.0.72.0/22",
+
+                // IPv6
+                "2400:cb00::/32",
+                "2606:4700::/32",
+                "2803:f800::/32",
+                "2405:b500::/32",
+                "2405:8100::/32",
+                "2a06:98c0::/29",
+                "2c0f:f248::/32",
+                ];
+        }
     }
 
     private static readonly string[] DefaultProxies = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"];
