dbdcb9acaff3eef80dc57c588a950bb6a208d90c
hhvrc <github@heavenvr.tech>
2024-09-30 10:10:36 +0200

    Improve RandomString method by using RandomNumberGenerator

--- API/Utils/CryptoUtils.cs
+++ API/Utils/CryptoUtils.cs
@@ -1,26 +1,8 @@
 ﻿using System.Security.Cryptography;
-using System.Text;
 
 namespace OpenShock.API.Utils;
 
 public static class CryptoUtils
 {
-    public static string RandomString(int length)
-    {
-        const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        var res = new StringBuilder();
-        using (var rng = RandomNumberGenerator.Create())
-        {
-            var uintBuffer = new byte[sizeof(uint)];
-
-            while (length-- > 0)
-            {
-                rng.GetBytes(uintBuffer);
-                var num = BitConverter.ToUInt32(uintBuffer, 0);
-                res.Append(valid[(int)(num % (uint)valid.Length)]);
-            }
-        }
-
-        return res.ToString();
-    }
+    public static string RandomString(int length) => RandomNumberGenerator.GetString("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", length);
 }
