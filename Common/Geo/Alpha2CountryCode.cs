ecf5938a9482a6841fa8265ab72457d28c1d6aad
HeavenVR <github@heavenvr.tech>
2024-10-05 21:10:34 +0200

    Add tests (#116)
    * Add Common.Tests
    
    * Write more tests and update code

--- Common/Geo/Alpha2CountryCode.cs
+++ Common/Geo/Alpha2CountryCode.cs
@@ -10,7 +10,7 @@ public readonly record struct Alpha2CountryCode(char Char1, char Char2)
     {
         if (str.Length != 2 || !char.IsAsciiLetterUpper(str[0]) || !char.IsAsciiLetterUpper(str[1]))
         {
-            code = default;
+            code = UnknownCountry;
             return false;
         }
 
@@ -22,15 +22,28 @@ public readonly record struct Alpha2CountryCode(char Char1, char Char2)
     public static implicit operator Alpha2CountryCode(string str)
     {
         if (str.Length != 2)
-            throw new ArgumentOutOfRangeException(nameof(str), "String input must be exactly 2 chars");
+            throw new ArgumentOutOfRangeException(nameof(str), "Country code must be exactly 2 characters long");
 
         if (!char.IsAsciiLetterUpper(str[0]) || !char.IsAsciiLetterUpper(str[1]))
-            throw new ArgumentOutOfRangeException(nameof(str), "String input must be upper characters only");
+            throw new ArgumentOutOfRangeException(nameof(str), "Country code must be uppercase ASCII characters only");
 
         return new Alpha2CountryCode(str[0], str[1]);
     }
 
+    public static int GetCombinedHashCode(Alpha2CountryCode code1, Alpha2CountryCode code2)
+    {
+        int a = code1.GetHashCode();
+        int b = code2.GetHashCode();
+
+        int v = (a << 16) | b;
+
+        if (a > b) v = int.RotateLeft(v, 16);
+
+        return v;
+    }
+
     public bool IsUnknown() => this == UnknownCountry;
 
+    public override int GetHashCode() => (Char1 << 8) | Char2;
     public override string ToString() => new([Char1, Char2]);
 }
