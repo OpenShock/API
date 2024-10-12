a4614d6a4faa4e85bff269852b612cbf4cac1e41
Luca <luca@perugia.ovh>
2024-10-13 00:28:14 +0200

    Add migrations

---
+++ Common/Migrations/20241012222620_Add username change table.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace OpenShock.Common.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addusernamechangetable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users_email_changes",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    used_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    secret = table.Column<string>(type: "character varying", nullable: false),
+                    email = table.Column<string>(type: "character varying", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_email_change_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users_name_changes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    old_name = table.Column<string>(type: "character varying", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_name_changes_pkey", x => new { x.id, x.user_id });
+                    table.ForeignKey(
+                        name: "fk_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "idx_email_changes_created_on",
+                table: "users_email_changes",
+                column: "created_on")
+                .Annotation("Npgsql:StorageParameter:deduplicate_items", "true");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_email_changes_used_on",
+                table: "users_email_changes",
+                column: "used_on")
+                .Annotation("Npgsql:StorageParameter:deduplicate_items", "true");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email_changes_user_id",
+                table: "users_email_changes",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_user_name_changes_created_on",
+                table: "users_name_changes",
+                column: "created_on")
+                .Annotation("Npgsql:StorageParameter:deduplicate_items", "true");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_user_name_changes_old_name",
+                table: "users_name_changes",
+                column: "old_name")
+                .Annotation("Npgsql:StorageParameter:deduplicate_items", "true");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_name_changes_user_id",
+                table: "users_name_changes",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_user_id",
+                table: "api_tokens");
+
+            migrationBuilder.DropTable(
+                name: "users_email_changes");
+
+            migrationBuilder.DropTable(
+                name: "users_name_changes");
+        }
+    }
+}
