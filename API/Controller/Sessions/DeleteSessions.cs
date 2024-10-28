f7d6bc69fcbb46751e3ab9b426465324ced9efa7
hhvrc <github@heavenvr.tech>
2024-10-28 12:46:40 +0100

    Allow admins to delete sessions by id

--- API/Controller/Sessions/DeleteSessions.cs
+++ API/Controller/Sessions/DeleteSessions.cs
@@ -12,10 +12,16 @@ public sealed partial class SessionsController
     [ProducesProblem(HttpStatusCode.NotFound, "SessionNotFound")]
     public async Task<IActionResult> DeleteSession(Guid sessionId)
     {
-        var result = await _sessionService.DeleteSession(CurrentUser.DbUser.Id, sessionId);
+        var loginSession = await _sessionService.GetSession(sessionId);
 
-        return result.Match(
-            success => RespondSlimSuccess(),
-            error => Problem(SessionError.SessionNotFound));
+        // If the session was not found, or does not belong to the current user (unless its an admin) return NotFound
+        if (loginSession == null || (loginSession.UserId != CurrentUser.DbUser.Id && CurrentUser.DbUser.Rank < Common.Models.RankType.Admin)) 
+        {
+            return Problem(SessionError.SessionNotFound);
+        }
+
+        await _sessionService.DeleteSession(loginSession);
+
+        return RespondSlimSuccess();
     }
 }
