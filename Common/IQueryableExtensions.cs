34856580bbb0bbe4a169dc3c9df6e399cbec15fe
hhvrc <github@heavenvr.tech>
2024-10-28 14:50:00 +0100

    Try to simplify predicate logic

--- Common/IQueryableExtensions.cs
+++ Common/IQueryableExtensions.cs
@@ -6,52 +6,60 @@ namespace OpenShock.Common;
 
 public static class IQueryableExtensions
 {
-    public static IQueryable<TEntity> WhereIsUserOrRank<TEntity>(this IQueryable<TEntity> source, Expression<Func<TEntity, OpenShockDb.User>> navigationSelector, Guid userId, RankType rank)
+    private static Expression<Func<TEntity, bool>> EntityPropExpr<TEntity>(Expression navigationSelector, Func<InvocationExpression, BinaryExpression> binaryExpressionGetter)
     {
         // Create a parameter for TEntity (entity)
         var entityParameter = Expression.Parameter(typeof(TEntity), "entity");
 
-        // Access the user from the navigation selector
-        var userExpression = Expression.Invoke(navigationSelector, entityParameter);
+        // Access the prop from the navigation selector
+        var entityExpression = Expression.Invoke(navigationSelector, entityParameter);
 
-        // Build expressions to check user ID and rank
-        var userIdCheck = Expression.Equal(
+        var binaryExpression = binaryExpressionGetter(entityExpression);
+
+        // Build the lambda expression
+        return Expression.Lambda<Func<TEntity, bool>>(binaryExpression, entityParameter);
+    }
+
+    private static BinaryExpression UserIdMatchesExpr(InvocationExpression userExpression, Guid userId)
+    {
+        return Expression.Equal(
             Expression.Property(userExpression, nameof(OpenShockDb.User.Id)),
             Expression.Constant(userId)
         );
+    }
 
-        var userRankCheck = Expression.GreaterThanOrEqual(
-            Expression.Property(userExpression, nameof(OpenShockDb.User.Rank)),
-            Expression.Constant(rank)
-        );
+    private static Expression<Func<TEntity, bool>> IsUserMatchExpr<TEntity>(Expression navigationSelector, Guid userId)
+    {
+        return EntityPropExpr<TEntity>(navigationSelector, user => UserIdMatchesExpr(user, userId));
+    }
 
-        // Combine the checks using "AND" (userId == userId && userRank >= requiredRank)
-        var combinedCheck = Expression.AndAlso(userIdCheck, userRankCheck);
-
-        // Build the lambda expression
-        var predicate = Expression.Lambda<Func<TEntity, bool>>(combinedCheck, entityParameter);
-
-        // Apply the predicate to the source IQueryable
-        return source.Where(predicate);
+    public static IQueryable<TEntity> WhereIsUser<TEntity>(this IQueryable<TEntity> source, Expression navigationSelector, Guid userId)
+    {
+        return source.Where(IsUserMatchExpr<TEntity>(navigationSelector, userId));
     }
 
     public static IQueryable<TEntity> WhereIsUserOrRank<TEntity>(this IQueryable<TEntity> source, Expression<Func<TEntity, OpenShockDb.User>> navigationSelector, OpenShockDb.User user, RankType rank)
     {
-        return WhereIsUserOrRank(source, navigationSelector, user.Id, rank);
+        if (user.Rank >= rank)
+        {
+            return source;
+        }
+
+        return WhereIsUser(source, navigationSelector, user.Id);
     }
 
     public static IQueryable<TEntity> WhereIsUserOrRank<TEntity>(this IQueryable<TEntity> source, Expression<Func<TEntity, OpenShockDb.User>> navigationSelector, LinkUser user, RankType rank)
     {
-        return WhereIsUserOrRank(source, navigationSelector, user.DbUser.Id, rank);
+        return WhereIsUserOrRank(source, navigationSelector, user.DbUser, rank);
     }
 
     public static IQueryable<TEntity> WhereIsUserOrAdmin<TEntity>(this IQueryable<TEntity> source, Expression<Func<TEntity, OpenShockDb.User>> navigationSelector, OpenShockDb.User user)
     {
-        return WhereIsUserOrRank(source, navigationSelector, user.Id, RankType.Admin);
+        return WhereIsUserOrRank(source, navigationSelector, user, RankType.Admin);
     }
 
     public static IQueryable<TEntity> WhereIsUserOrAdmin<TEntity>(this IQueryable<TEntity> source, Expression<Func<TEntity, OpenShockDb.User>> navigationSelector, LinkUser user)
     {
-        return WhereIsUserOrRank(source, navigationSelector, user.DbUser.Id, RankType.Admin);
+        return WhereIsUserOrRank(source, navigationSelector, user.DbUser, RankType.Admin);
     }
 }
