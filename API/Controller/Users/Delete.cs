f7b2721908baa4b8f7114b6f9201b9e65490a8c6
hhvrc <github@heavenvr.tech>
2024-10-28 22:02:30 +0100

    Add Users Delete Self endpoint

---
+++ API/Controller/Users/Delete.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenShock.Common.Errors;
+using OpenShock.Common.Models;
+
+namespace OpenShock.API.Controller.Users;
+
+public sealed partial class UsersController
+{
+    /// <summary>
+    /// Delete the current user
+    /// </summary>
+    /// <response code="200">The user was successfully deleted.</response>
+    [HttpGet("self")]
+    public async Task<IActionResult> DeleteSelf()
+    {
+        if (CurrentUser.IsRank(RankType.Admin))
+        {
+            return Problem(AdminError.CannotDeletePrivledgedAccount);
+        }
+
+        _db.Users.Remove(CurrentUser.DbUser);
+        await _db.SaveChangesAsync();
+
+        return RespondSlimSuccess();
+    }
+}
