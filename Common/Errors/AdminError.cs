422abbb0ad944a36fcfd11d89d173b34491b377c
hhvrc <github@heavenvr.tech>
2024-10-28 20:48:41 +0100

    Add Admin DeleteUser endpoint

---
+++ Common/Errors/AdminError.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using OpenShock.Common.Problems;
+
+namespace OpenShock.Common.Errors;
+
+public static class AdminError
+{
+    public static OpenShockProblem CannotDeletePrivledgedAccount => new OpenShockProblem("User.Privileged.DeleteDenied",
+        "You cannot delete a privileged user", HttpStatusCode.Forbidden);
+    
+    public static OpenShockProblem UserNotFound => new("User.NotFound", "User not found", HttpStatusCode.NotFound);
+}
