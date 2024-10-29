7de30979e4e266b1eaad558521ac929294614447
Luca <luca@perugia.ovh>
2024-10-29 15:20:23 +0100

    Get shares endpoints

--- Common/Errors/ShareError.cs
+++ Common/Errors/ShareError.cs
@@ -8,4 +8,6 @@ public static class ShareError
 {
     public static OpenShockProblem ShareCreateCannotShareWithSelf => new("Share.Create.CannotShareWithSelf", "You cannot share something with yourself", HttpStatusCode.BadRequest);
     public static ShockersNotFoundProblem ShareCreateShockerNotFound(IEnumerable<Guid> missingShockers) => new("Share.Create.ShockerNotFound", "One or multiple of the provided shocker's were not found or do not belong to you", missingShockers, HttpStatusCode.NotFound);
+    
+    public static OpenShockProblem ShareGetNoShares => new("Share.Get.NoShares", "You have no shares with the specified user, or the user doesnt exist", HttpStatusCode.NotFound);
 }
