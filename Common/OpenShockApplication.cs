8dc0143b1a951f62a121cacf24c4c3068dc5f75a
HeavenVR <github@heavenvr.tech>
2024-11-26 19:26:44 +0100

    Specifically only add back whats needed for https in debug mode

--- Common/OpenShockApplication.cs
+++ Common/OpenShockApplication.cs
@@ -27,7 +27,7 @@ public static class OpenShockApplication
         });
 
         // Since we use slim builders, this allows for HTTPS during local development
-        if (isDevelopment) builder.WebHost.UseKestrel();
+        if (isDevelopment) builder.WebHost.UseKestrelHttpsConfiguration();
         
         builder.WebHost.ConfigureKestrel(serverOptions =>
         {
