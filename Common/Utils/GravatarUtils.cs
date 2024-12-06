b0f57d5f35cb1b854c4f1ab4e239755e9275eb13
hhvrc <github@heavenvr.tech>
2024-12-06 15:36:54 +0100

    Use optimized LowerCase HexString methods

--- Common/Utils/GravatarUtils.cs
+++ Common/Utils/GravatarUtils.cs
@@ -7,7 +7,7 @@ public static class GravatarUtils
 {
     private static readonly string DefaultImageUrl = HttpUtility.UrlEncode("https://openshock.app/static/images/Icon512.png");
 
-    public static Uri GetImageUrl(string email) => new Uri($"https://www.gravatar.com/avatar/{HashingUtils.HashSha256(email).ToLowerInvariant()}?d={DefaultImageUrl}");
+    public static Uri GetImageUrl(string email) => new Uri($"https://www.gravatar.com/avatar/{HashingUtils.HashSha256(email)}?d={DefaultImageUrl}");
 
     public static Uri GetImage(this User user) => GetImageUrl(user.Email);
 }
