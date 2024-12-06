5068921fe529a9676e861b3ff2093d81c69ae7e8
Luca <luca@perugia.ovh>
2024-12-06 15:26:28 +0100

    fix tests

--- Common.Tests/Utils/HashingUtilsTests.cs
+++ Common.Tests/Utils/HashingUtilsTests.cs
@@ -5,7 +5,7 @@ namespace OpenShock.Common.Tests.Utils;
 public class HashingUtilsTests
 {
     [Test]
-    [Arguments("test", "9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08")]
+    [Arguments("test", "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08")]
     public async Task HashSha256(string str, string expectedHash)
     {
         // Act
