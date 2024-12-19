1aa73e56e120ee0f9636150f83db8783acd6e5e3
hhvrc <github@heavenvr.tech>
2024-12-19 13:14:02 +0100

    Fix admin_users_view.email_actived still existing

---
+++ Common/Migrations/20241219115917_FixAdminUsersView.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenShock.Common.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAdminUsersView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                IF EXISTS(SELECT *
+                    FROM information_schema.columns
+                    WHERE table_name='admin_users_view' and column_name='email_actived')
+                THEN
+                    ALTER VIEW admin_users_view RENAME COLUMN email_actived TO email_activated;
+                END IF;
+                """
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER VIEW admin_users_view RENAME COLUMN email_activated TO email_actived");
+        }
+    }
+}
