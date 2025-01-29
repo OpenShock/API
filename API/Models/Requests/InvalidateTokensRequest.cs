f2e67a8e2eeb344ba4962b510b839d7a343054be
hhvrc <github@heavenvr.tech>
2025-01-29 15:09:20 +0100

    Rename and add functionality to ReportTokens endpoint

--- API/Models/Requests/InvalidateTokensRequest.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace OpenShock.API.Models.Requests;
-
-public class InvalidateTokensRequest
-{
-    public required IEnumerable<string> Secrets { get; set; }
-}
---
+++ Common/OpenShockDb/ApiTokenReport.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace OpenShock.Common.OpenShockDb;
+
+public class ApiTokenReport
+{
+    public Guid Id { get; set; }
+
+    public DateTime ReportedAt { get; set; }
+
+    public Guid ReportedByUserId { get; set; }
+
+    public IPAddress ReportedByIp { get; set; } = null!;
+
+    public virtual User ReportedByUser { get; set; } = null!;
+}
