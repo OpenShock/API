168e573ba16a00c6082fc9e313df5f550d75e984
hhvrc <github@heavenvr.tech>
2025-02-06 00:26:13 +0100

    Add claims for Hub authentication

--- Common/Authentication/AuthenticationHandlers/HubAuthentication.cs
+++ Common/Authentication/AuthenticationHandlers/HubAuthentication.cs
@@ -42,20 +42,24 @@ public sealed class HubAuthentication : AuthenticationHandler<AuthenticationSche
 
     protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
     {
-        if (!Context.TryGetDeviceTokenFromHeader(out string? sessionKey))
+        if (!Context.TryGetDeviceTokenFromHeader(out var sessionKey))
         {
             return Fail(AuthResultError.HeaderMissingOrInvalid);
         }
 
-        var device = await _db.Devices.Where(x => x.Token == sessionKey).FirstOrDefaultAsync();
-        if (device == null) return Fail(AuthResultError.TokenInvalid);
+        var device = await _db.Devices.FirstOrDefaultAsync(x => x.Token == sessionKey);
+        if (device == null)
+        {
+            return Fail(AuthResultError.TokenInvalid);
+        }
 
         _authService.CurrentClient = device;
-        Context.Items["Device"] = _authService.CurrentClient.Id;
 
         var claims = new[]
         {
-            new Claim("id", _authService.CurrentClient.Id.ToString()),
+            new(ClaimTypes.AuthenticationMethod, OpenShockAuthSchemas.HubToken),
+            new Claim(ClaimTypes.NameIdentifier, device.Owner.ToString()),
+            new Claim(OpenShockAuthClaims.HubId, _authService.CurrentClient.Id.ToString()),
         };
         var ident = new ClaimsIdentity(claims, nameof(HubAuthentication));
         var ticket = new AuthenticationTicket(new ClaimsPrincipal(ident), Scheme.Name);
