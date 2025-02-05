85294cb5480516f93664a4bae95918f9de04088a
LucHeart <luc@luc.cat>
2025-02-05 19:44:53 +0100

    Fix admin user view

--- Common/OpenShockDb/OpenShockContext.cs
+++ Common/OpenShockDb/OpenShockContext.cs
@@ -579,7 +579,8 @@ public partial class OpenShockContext : DbContext
                 .HasColumnName("email_activated");
             entity.Property(e => e.Roles)
                 .HasColumnType("role_type[]")
-                .HasColumnName("roles");
+                .HasColumnName("roles")
+                .HasConversion(x => x.ToArray(), x => x.ToList());
             entity.Property(e => e.ApiTokenCount)
                 .HasColumnName("api_token_count");
             entity.Property(e => e.PasswordResetCount)
