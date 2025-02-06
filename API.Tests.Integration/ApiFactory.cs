5048fd36e6a999c6ddc0390a5e676208e98d6f64
hhvrc <github@heavenvr.tech>
2025-02-06 11:35:45 +0100

    Create API integration tests project

---
+++ API.Tests.Integration/ApiFactory.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace OpenShock.API.Tests.Integration;
+
+internal class ApiFactory : WebApplicationFactory<Program>
+{
+
+}
---
+++ API.Tests.Integration/BasicTests.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace OpenShock.API.Tests.Integration;
+
+internal class BasicTests : IClassFixture<WebApplicationFactory<Program>>
+{
+}
