34263d5d53d2037aa8a674b721771eb770c2af78
hhvrc <github@heavenvr.tech>
2025-02-10 17:14:49 +0100

    Start creating tests for query string tokenizer

---
+++ Common.Tests/Query/QueryStringTokenizerTests.cs
@@ -0,0 +1,45 @@
+﻿using OpenShock.Common.Query;
+
+namespace OpenShock.Common.Tests.Query;
+public class QueryStringTokenizerTests
+{
+    [Test]
+    public async Task SimpleString_ReturnsMatching()
+    {
+        // Act
+        var result = QueryStringTokenizer.ParseQueryTokens("testing");
+
+        // Assert
+        await Assert.That(result).IsEqualTo(["testing"]);
+    }
+
+    [Test]
+    public async Task SpaceSeperatedString_ReturnsMatching()
+    {
+        // Act
+        var result = QueryStringTokenizer.ParseQueryTokens("testing tokenizer");
+
+        // Assert
+        await Assert.That(result).IsEqualTo(["testing", "tokenizer"]);
+    }
+
+    [Test]
+    public async Task QuotedString_ReturnsMatching()
+    {
+        // Act
+        var result = QueryStringTokenizer.ParseQueryTokens("'testing tokenizer'");
+
+        // Assert
+        await Assert.That(result).IsEqualTo(["testing tokenizer"]);
+    }
+
+    [Test]
+    public async Task QuotedAndEscapedString_ReturnsMatching()
+    {
+        // Act
+        var result = QueryStringTokenizer.ParseQueryTokens("'this shouldn\'t fail'");
+
+        // Assert
+        await Assert.That(result).IsEqualTo(["this shouldn't fail"]);
+    }
+}
