ca5531fc1fe562a2e8abb5e2d7110b2da9e452eb
HeavenVR <github@heavenvr.tech>
2025-02-11 15:21:24 +0100

    Query ExpressionBuilder BugFixing (#160)
    * Implement proper quoted string parser
    
    * Further optimize and abstract out filter query gen
    
    * Start creating tests for query string tokenizer
    
    * Add empty string test
    
    * Fix assertions
    
    * Fix off-by-one bug
    
    * Couple fixes
    
    * More test and bugfixes
    
    * Fix more bugs, add more tests
    
    * And more fixes
    
    * Add Bogus
    
    * Fix tests

--- Common/Errors/ExpressionError.cs
+++ Common/Errors/ExpressionError.cs
@@ -5,5 +5,6 @@ namespace OpenShock.Common.Errors;
 
 public static class ExpressionError
 {
+    public static OpenShockProblem QueryStringInvalidError(string details) => new OpenShockProblem("ExpressionError", "Query string is invalid", HttpStatusCode.BadRequest, details);
     public static OpenShockProblem ExpressionExceptionError(string details) => new OpenShockProblem("ExpressionError", "An error occured while processing the expression", HttpStatusCode.BadRequest, details);
 }
