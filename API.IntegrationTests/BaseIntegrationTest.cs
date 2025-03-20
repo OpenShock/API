0e5fe9036be522abb9a618f02a381a2b44a7e58d
hhvrc <github@heavenvr.tech>
2025-03-20 16:49:24 +0100

    Make first actual integration test

--- API.IntegrationTests/BaseIntegrationTest.cs
+++ API.IntegrationTests/BaseIntegrationTest.cs
@@ -1,30 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using OpenShock.API.Services.Account;
-
-namespace API.IntegrationTests;
+﻿namespace API.IntegrationTests;
 
 public abstract class BaseIntegrationTest
 {
-    public BaseIntegrationTest()
-    {
-        Console.WriteLine("Constructor!");
-    }
-
-    [ClassDataSource<IntegrationTestWebAppFactory>(Shared = SharedType.PerAssembly)]
+    [ClassDataSource<IntegrationTestWebAppFactory>(Shared = SharedType.PerTestSession)]
     public required IntegrationTestWebAppFactory WebAppFactory { get; init; }
-    public IServiceScope ServiceScope { get; set; }
-
-    public IAccountService AccountService { get; set; }
-
-    [Before(Test)]
-    public Task Initialize()
-    {
-        Console.WriteLine("Initialize!");
-
-        ServiceScope = WebAppFactory.Services.CreateScope();
-
-        AccountService = ServiceScope.ServiceProvider.GetRequiredService<IAccountService>();
-
-        return Task.CompletedTask;
-    }
 }
