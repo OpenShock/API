b59bb16d74270d7dea4a7475f7e79d6d0eff416a
HeavenVR <github@heavenvr.tech>
2025-03-21 08:38:57 +0100

    Create API integration tests (#158)
    * Create API integration tests project
    
    * Start implementing proper integration tests
    
    * Fix some issues
    
    * Get IntegrationTestWebAppFactory working
    
    * Start messing around with tests
    
    * Swap out xUnit with TUnit
    
    * Make first actual integration test
    
    * Use shared props for API IntegrationTests

---
+++ API.IntegrationTests/BaseIntegrationTest.cs
@@ -0,0 +1,7 @@
+﻿namespace API.IntegrationTests;
+
+public abstract class BaseIntegrationTest
+{
+    [ClassDataSource<IntegrationTestWebAppFactory>(Shared = SharedType.PerTestSession)]
+    public required IntegrationTestWebAppFactory WebAppFactory { get; init; }
+}
