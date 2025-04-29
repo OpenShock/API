a16017e4a391279f47b6a4a501e19ee062199805
hhvrc <github@heavenvr.tech>
2025-04-29 12:26:02 +0200

    Specify servers in OpenAPI docs

--- Common/OpenApi/OpenApiDocumentTransformer.cs
+++ Common/OpenApi/OpenApiDocumentTransformer.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.OpenApi;
 using Microsoft.OpenApi.Models;
+using OpenShock.Common.Authentication;
+using OpenShock.Common.Constants;
 
 namespace OpenShock.Common.OpenApi;
 
-public sealed class OpenApiDocumentTransformer : IOpenApiDocumentTransformer
+public sealed class OpenApiDocumentTransformer<TProgram> : IOpenApiDocumentTransformer where TProgram : class
 {
     public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
     {
+        document.Servers = [
+#if DEBUG
+            new OpenApiServer { Url = "https://localhost" },
+#endif
+            new OpenApiServer { Url = "https://api.openshock.app" },
+            new OpenApiServer { Url = "https://api.openshock.dev" }
+        ];
+
         return Task.CompletedTask;
     }
 }
