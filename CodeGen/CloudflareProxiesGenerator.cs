b1c21645944e9ade7d42ade8a18d18f473e7a50d
hhvrc <github@heavenvr.tech>
2025-05-08 12:15:11 +0200

    Update CloudflareProxiesGenerator.cs

--- CodeGen/CloudflareProxiesGenerator.cs
+++ CodeGen/CloudflareProxiesGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace OpenShock.CodeGen;
@@ -10,6 +11,39 @@ internal class CloudflareProxiesGenerator : IIncrementalGenerator
 {
     private const string IpV4Name = "cloudflare-ips-v4.txt";
     private const string IpV6Name = "cloudflare-ips-v6.txt";
+
+    private static bool TryParseAndWriteIpNetworkArrayEntry(StringBuilder builder, string ipNetwork, AddressFamily addressFamily)
+    {
+        if (string.IsNullOrEmpty(ipNetwork)) return false;
+
+        int slash = ipNetwork.IndexOf('/');
+        if (slash < 1 || slash == ipNetwork.Length - 1) return false;
+
+        // split into address/CIDR without extra array allocations
+        string addressPart = ipNetwork.Substring(0, slash);
+        string cidrPart = ipNetwork.Substring(slash + 1);
+
+        if (!ushort.TryParse(cidrPart, out var cidr) || !IPAddress.TryParse(addressPart, out var ipAddress)) return false;
+
+        if (ipAddress.AddressFamily != addressFamily) return false;
+
+        var bytes = ipAddress.GetAddressBytes();
+
+        builder.Append("new IPNetwork(new IPAddress(new byte[]{");
+
+        for (int i = 0, last = bytes.Length - 1; i <= last; i++)
+        {
+            builder.Append(bytes[i]);
+            if (i < last)
+                builder.Append(',');
+        }
+
+        builder.Append("}),");
+        builder.Append(cidr);
+        builder.AppendLine("),");
+
+        return true;
+    }
 
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
@@ -31,46 +65,34 @@ internal class CloudflareProxiesGenerator : IIncrementalGenerator
 
             foreach (var line in ipv4Lines)
             {
-                if (!IPAddress.TryParse(line, out _))
+                if (!TryParseAndWriteIpNetworkArrayEntry(sourceBuilder, line, AddressFamily.InterNetwork))
                 {
                     ctx.ReportDiagnostic(Diagnostic.Create(
                         new DiagnosticDescriptor(
                             "CF001",
-                            "Invalid IP Address",
-                            $"The entry '{line}' in {IpV4Name} is not a valid IP Address.",
+                            "Invalid IPv4 Address",
+                            $"The entry '{line}' in {IpV4Name} is not a valid IPv4 Address.",
                             "CloudflareProxiesGenerator",
                             DiagnosticSeverity.Error,
                             true),
                         Location.None));
-
-                    continue; // Skip invalid entry
                 }
-
-                sourceBuilder.Append("    IPNetwork.Parse(\"");
-                sourceBuilder.Append(line);
-                sourceBuilder.AppendLine("\"),");
             }
             sourceBuilder.AppendLine("\n    // IPv6");
             foreach (var line in ipv6Lines)
             {
-                if (!IPAddress.TryParse(line, out _))
+                if (!TryParseAndWriteIpNetworkArrayEntry(sourceBuilder, line, AddressFamily.InterNetworkV6))
                 {
                     ctx.ReportDiagnostic(Diagnostic.Create(
                         new DiagnosticDescriptor(
-                            "CF001",
-                            "Invalid IP Address",
-                            $"The entry '{line}' in {IpV6Name} is not a valid IP Address.",
+                            "CF002",
+                            "Invalid IPv6 Address",
+                            $"The entry '{line}' in {IpV6Name} is not a valid IPv6 Address.",
                             "CloudflareProxiesGenerator",
                             DiagnosticSeverity.Error,
                             true),
                         Location.None));
-
-                    continue; // Skip invalid entry
                 }
-
-                sourceBuilder.Append("    IPNetwork.Parse(\"");
-                sourceBuilder.Append(line);
-                sourceBuilder.AppendLine("\"),");
             }
 
             sourceBuilder.AppendLine("  ];\n}");
