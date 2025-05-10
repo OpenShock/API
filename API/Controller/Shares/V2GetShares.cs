f846a876293b893285e15b1c5a76d9a82136ceb5
LucHeart <luc@luc.cat>
2025-05-10 17:37:58 +0200

    Share stuffs

--- API/Controller/Shares/V2GetShares.cs
+++ API/Controller/Shares/V2GetShares.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OpenShock.API.Models.Response;
 using OpenShock.Common.Extensions;
+using Z.EntityFramework.Plus;
 
 namespace OpenShock.API.Controller.Shares;
 
@@ -10,16 +11,16 @@ public sealed partial class SharesController
 {
     [HttpGet]
     [ApiVersion("2")]
-    public IAsyncEnumerable<V2UserSharesListItem> GetSharesByUsers()
+    public async Task<V2UserShares> GetSharesByUsers(CancellationToken cancellationToken)
     {
-        return _db.ShockerShares
+        var sharedWithOthersFuture = _db.ShockerShares
             .Where(x => x.Shocker.DeviceNavigation.Owner == CurrentUser.Id)
             .AsNoTracking()
             .GroupBy(x => x.SharedWith)
-            .Select(g => new V2UserSharesListItem
+            .Select(g => new V2UserSharesListItemDto
             {
                 Id = g.Key,
-                Image = g.First().SharedWithNavigation.GetImageUrl(),
+                Email = g.First().SharedWithNavigation.Email,
                 Name = g.First().SharedWithNavigation.Name,
                 Shares = g.Select(y => new UserShareInfo
                     {
@@ -42,6 +43,50 @@ public sealed partial class SharesController
                     })
                     .ToArray()
             })
-            .AsAsyncEnumerable();
+            .Future();
+
+        var sharedWithMeFuture = _db.ShockerShares
+            .Where(x => x.SharedWith == CurrentUser.Id)
+            .AsNoTracking()
+            .GroupBy(x => x.Shocker.DeviceNavigation.Owner)
+            .Select(g => new V2UserSharesListItemDto
+            {
+                Id = g.Key,
+                Email = g.First().Shocker.DeviceNavigation.OwnerNavigation.Email,
+                Name = g.First().Shocker.DeviceNavigation.OwnerNavigation.Name,
+                Shares = g.Select(y => new UserShareInfo
+                    {
+                        Id = y.Shocker.Id,
+                        Name = y.Shocker.Name,
+                        CreatedOn = y.CreatedOn,
+                        Permissions = new ShockerPermissions
+                        {
+                            Sound = y.PermSound,
+                            Vibrate = y.PermVibrate,
+                            Shock = y.PermShock,
+                            Live = y.PermLive
+                        },
+                        Limits = new ShockerLimits
+                        {
+                            Duration = y.LimitDuration,
+                            Intensity = y.LimitIntensity
+                        },
+                        Paused = y.Paused
+                    })
+                    .ToArray()
+            })
+            .Future();
+
+        return new V2UserShares
+        {
+            SharedWithMe = (await sharedWithMeFuture.ToArrayAsync(cancellationToken)).Select(x => x.FromDto()),
+            SharedWithOthers = (await sharedWithOthersFuture.ToArrayAsync(cancellationToken)).Select(x => x.FromDto())
+        };
+    }
+
+    public sealed class V2UserShares
+    {
+        public required IEnumerable<V2UserSharesListItem> SharedWithMe { get; set; }
+        public required IEnumerable<V2UserSharesListItem> SharedWithOthers { get; set; }
     }
 }
