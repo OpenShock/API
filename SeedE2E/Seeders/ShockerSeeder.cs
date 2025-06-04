e97ad55a6d0e4111881c3c4d7d0d87ec4219dd70
hhvrc <github@heavenvr.tech>
2025-06-04 12:32:49 +0200

    Make ShockerModelTypes entried fetch oly run once

--- SeedE2E/Seeders/ShockerSeeder.cs
+++ SeedE2E/Seeders/ShockerSeeder.cs
@@ -8,6 +8,8 @@ namespace OpenShock.SeedE2E.Seeders;
 
 public static class ShockerSeeder
 {
+    private static readonly ShockerModelType[] ShockerModelTypes = Enum.GetValues<ShockerModelType>().Cast<ShockerModelType>().ToArray();
+
     public static async Task SeedAsync(OpenShockContext db)
     {
         // Only seed if there are no shockers yet
@@ -21,7 +23,7 @@ public static class ShockerSeeder
             .RuleFor(s => s.Id, f => Guid.CreateVersion7())
             .RuleFor(s => s.DeviceId, f => f.PickRandom(allDeviceIds))
             .RuleFor(s => s.Name, f => f.Commerce.ProductAdjective().Truncate(HardLimits.ShockerNameMaxLength))
-            .RuleFor(s => s.Model, f => f.PickRandom(Enum.GetValues<ShockerModelType>().Cast<ShockerModelType>()))
+            .RuleFor(s => s.Model, f => f.PickRandom(ShockerModelTypes))
             .RuleFor(s => s.RfId, f => f.Random.UShort())
             .RuleFor(s => s.IsPaused, f => f.Random.Bool(0.2f))
             .RuleFor(s => s.CreatedAt, f => f.Date.RecentOffset(30).UtcDateTime);
