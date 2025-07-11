16af3e0cf26cef27dcd6d017819f2eedbe3e0850
hhvrc <github@heavenvr.tech>
2025-07-11 13:40:51 +0200

    Improve CreateToken response DTO

--- API/Models/Response/TokenCreatedResponse.cs
+++ API/Models/Response/TokenCreatedResponse.cs
@@ -1,7 +1,20 @@
-﻿namespace OpenShock.API.Models.Response;
+﻿using OpenShock.Common.Models;
+
+namespace OpenShock.API.Models.Response;
 
 public sealed class TokenCreatedResponse
 {
+    public required Guid Id { get; set; 
+
+    public required string Name { get; set; }
+
     public required string Token { get; set; }
-    public required Guid Id { get; set; }
+
+    public required DateTime CreatedAt { get; set; }
+
+    public required DateTime? ValidUntil { get; set; }
+
+    public required DateTime LastUsed { get; set; }
+
+    public required List<PermissionType> Permissions { get; set; }
 }
