6f34d5db2a631e36a45cdca06c48f54907955f8f
hhvrc <github@heavenvr.tech>
2025-07-30 17:45:30 +0200

    initial

---
+++ Common/OpenShockDb/EmailProviderBlacklist.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenShock.Common.OpenShockDb;
+
+public sealed class EmailProviderBlacklist
+{
+    public required string Domain { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+}
---
+++ Common/OpenShockDb/UserNameBlacklist.cs
@@ -0,0 +1,18 @@
+﻿namespace OpenShock.Common.OpenShockDb;
+
+public enum MatchType
+{
+    Exact,
+    Contains,
+}
+
+public sealed class UserNameBlacklist
+{
+    public required Guid Id { get; set; }
+
+    public string Value { get; set; } = null!;
+
+    public MatchType MatchType { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+}
