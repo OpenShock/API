0bc4673fc92c2c41c35b1cbcfd9c5044dbc0c708
hhvrc <github@heavenvr.tech>
2025-08-16 00:22:01 +0200

    Base64

--- Common/Services/RedisPubSub/RedisPubService.cs
+++ Common/Services/RedisPubSub/RedisPubService.cs
@@ -14,52 +14,41 @@ public sealed class RedisPubService : IRedisPubService
         _subscriber = connectionMultiplexer.GetSubscriber();
     }
 
+    private Task<long> Publish<T>(T msg) => _subscriber.PublishAsync(RedisChannels.DeviceMessage,
+        Convert.ToBase64String(MessagePackSerializer.Serialize(msg)));
+
     public Task SendDeviceUpdate(Guid deviceId)
     {
-        var msg = DeviceMessage.Create(deviceId, DeviceTriggerType.DeviceInfoUpdated);
-        var bytes = MessagePackSerializer.Serialize(msg);
-        return _subscriber.PublishAsync(RedisChannels.DeviceMessage, bytes);
+        return Publish(DeviceMessage.Create(deviceId, DeviceTriggerType.DeviceInfoUpdated));
     }
 
     public Task SendDeviceOnlineStatus(Guid deviceId, bool isOnline)
     {
-        var msg = DeviceStatus.Create(deviceId, DeviceStatusType.Online);
-        var bytes = MessagePackSerializer.Serialize(msg);
-        return _subscriber.PublishAsync(RedisChannels.DeviceMessage, bytes);
+        return Publish(DeviceStatus.Create(deviceId, DeviceStatusType.Online));
     }
 
     public Task SendDeviceControl(Guid deviceId, Guid senderId, DeviceControlPayload.ShockerControlInfo[] controls)
     {
-        var msg = DeviceMessage.Create(deviceId, new DeviceControlPayload { Sender = senderId, Controls = controls });
-        var bytes = MessagePackSerializer.Serialize(msg);
-        return _subscriber.PublishAsync(RedisChannels.DeviceMessage, bytes);
+        return Publish(DeviceMessage.Create(deviceId, new DeviceControlPayload { Sender = senderId, Controls = controls }));
     }
 
     public Task SendDeviceCaptivePortal(Guid deviceId, bool enabled)
     {
-        var msg = DeviceMessage.Create(deviceId, DeviceToggleTarget.CaptivePortal, enabled);
-        var bytes = MessagePackSerializer.Serialize(msg);
-        return _subscriber.PublishAsync(RedisChannels.DeviceMessage, bytes);
+        return Publish(DeviceMessage.Create(deviceId, DeviceToggleTarget.CaptivePortal, enabled));
     }
 
     public Task SendDeviceEmergencyStop(Guid deviceId)
     {
-        var msg = DeviceMessage.Create(deviceId, DeviceTriggerType.DeviceEmergencyStop);
-        var bytes = MessagePackSerializer.Serialize(msg);
-        return _subscriber.PublishAsync(RedisChannels.DeviceMessage, bytes);
+        return Publish(DeviceMessage.Create(deviceId, DeviceTriggerType.DeviceEmergencyStop));
     }
 
     public Task SendDeviceOtaInstall(Guid deviceId, SemVersion version)
     {
-        var msg = DeviceMessage.Create(deviceId, new DeviceOtaInstallPayload { Version = version });
-        var bytes = MessagePackSerializer.Serialize(msg);
-        return _subscriber.PublishAsync(RedisChannels.DeviceMessage, bytes);
+        return Publish(DeviceMessage.Create(deviceId, new DeviceOtaInstallPayload { Version = version }));
     }
 
     public Task SendDeviceReboot(Guid deviceId)
     {
-        var msg = DeviceMessage.Create(deviceId, DeviceTriggerType.DeviceReboot);
-        var bytes = MessagePackSerializer.Serialize(msg);
-        return _subscriber.PublishAsync(RedisChannels.DeviceMessage, bytes);
+        return Publish(DeviceMessage.Create(deviceId, DeviceTriggerType.DeviceReboot));
     }
 }
