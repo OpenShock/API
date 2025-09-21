7c0166d19f0e9f35c9512592b57c7371bbf31da3
hhvrc <github@heavenvr.tech>
2025-09-21 15:53:48 +0200

    Remove Password from OAuthFinalize for now

--- API/Models/Requests/OAuthFinalizeRequest.cs
+++ API/Models/Requests/OAuthFinalizeRequest.cs
@@ -4,16 +4,12 @@ namespace OpenShock.API.Models.Requests;
 
 public sealed class OAuthFinalizeRequest
 {
-    /// <summary>Desired username (create only). If omitted, a name will be generated from the external profile.</summary>
+    /// <summary>
+    /// Desired username. If omitted, a name will be generated from the external profile.
+    /// </summary>
     [Username(true)]
     public required string? Username { get; init; }
     
     [EmailAddress(true)]
     public required string? Email { get; init; }
-
-    /// <summary>
-    /// New account password (create only).
-    /// </summary>
-    [Password(true)]
-    public required string? Password { get; init; }
 }
