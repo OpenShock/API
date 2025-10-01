894bcd8c1126ff9f2937f67d3f7f1beb640bae3c
HeavenVR <github@heavenvr.tech>
2025-10-01 12:56:00 +0200

    Fix some bugs

--- Common/Geo/Alpha2CountryCodeAttribute.cs
+++ Common/Geo/Alpha2CountryCodeAttribute.cs
@@ -8,17 +8,11 @@ public sealed class Alpha2CountryCodeAttribute : ValidationAttribute
     /// <inheritdoc/>
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        if (value is null)
-            return new ValidationResult("Value is null");
-
         if (value is not string asString)
             return new ValidationResult("Input type must be string");
 
-        if (asString.Length != 2)
-            return new ValidationResult("Input string must be exactly 2 characters long");
-
-        if (asString[0] is not > 'A' and < 'Z' || asString[1] is not > 'A' and < 'Z')
-            return new ValidationResult("Characters must be uppercase");
+        if (asString is not [>= 'A' and <= 'Z', >= 'A' and <= 'Z'])
+            return new ValidationResult("Input string must be exactly 2 uppercase characters");
 
         if (!Alpha2CountryCode.TryParse(asString, out var countryCode))
             return new ValidationResult($"Failed to create {nameof(Alpha2CountryCode)}");
